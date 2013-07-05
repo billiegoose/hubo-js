268b5e3df12cca63029642eea902d072e185a821
William Hilton <wmhilton@gmail.com>
2013-07-06 01:31:30 +0900

    Switched to using Jade instead of raw HTML. Both the source Jade and compiled HTML are tracked.

--- server.js
+++ server.js
@@ -1,20 +1,31 @@
-var http = require('http');
-var express = require('express');
-var path = require('path');
-var util = require('util');
-// Used by utils
-var fs = require('fs');
-
+// Cloud9 IDE provides an environment variable called PORT
 var port = process.env.PORT || 8125;
+// Valid values are 'development' and 'production'
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
+// Let's use Express.js, because everyone else does.
+var express = require('express');
 var app = express();
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.compress()); // Apply gzip compression
+app.use(express.bodyParser()); // Automatically parse data in http POST etc
+app.use(express.methodOverride()); // Provides faux support for http PUT and http DELETE methods
+app.use(app.router); // Lets us add code for handling specific paths
+app.use(express.directory('public')); // Provides cool directory indexes!
 
-var server = http.createServer(app);
-server.listen(port);
+// I have a simple strategy: each HTML page comes from a Jade page of the same name.
+// Using this connector is much easier than writing repetitive code to render 'views'.
+var connectJadeHtml = require('connect-jade-html');
+app.use(connectJadeHtml(
+	{ src: __dirname + '/views'
+	, dest: __dirname + '/public'
+	, pretty: true
+	, force: (process.env.NODE_ENV === 'development' ? true : false)
+	}
+));
+
+// Render static content
+app.use(express.static(__dirname + '/public'));
+
+// Start server
+app.listen(port);
 console.log('Server running at http://localhost:' + port + '/');
