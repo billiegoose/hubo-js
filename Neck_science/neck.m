49721d088191e215356be2dc302276372780b98f
William Hilton <wmhilton@gmail.com>
2013-07-29 20:50:09 +0900

    Fixed the neck kinematics, cause they were wrong before.

--- Neck_science/neck.m
+++ Neck_science/neck.m
@@ -6,7 +6,7 @@ clear; clc; close all;
 reset_neck_coordinates
 
 % Set up view
-view(26, 42);
+view(7, 14);
 axis([-30 30 -30 30 -100 30])
 daspect([1 1 1])
 xlabel('X')
@@ -20,46 +20,62 @@ nk2 = [0;0;0];
 neck1 = zeros(3,1,1);
 neck2 = zeros(3,1,1);
 
-pitch_range = -90:1:90;
-roll_range = -90:1:90;
+pitch_range = -45:1:45; %-90:1:90;
+roll_range = -45:1:45;
 
-for rd = roll_range %-90:1:90
-    ridx = rd + 91;
-    r = rd*pi/180;
-    RT = makehgtform('xrotate',r);
-for pd = pitch_range %-90:1:90
-    pidx = pd + 91;
-    reset_neck_coordinates
-    i = i + 1;
-    p = pd*pi/180;
+pitch_offset = find(pitch_range==0);
+roll_offset = find(roll_range==0);
+
+rindex = 0;
+pindex = 0;
+for pindex = 1:length(pitch_range)
+    % Create pitch matrix
+    p = pitch_range(pindex)*pi/180;
     PT = makehgtform('yrotate',p);
-    T = PT*RT;
-    T2 = RT*PT;
-    % assert(T==T2) Well, shucks.
-    T = T(1:3, 1:3);
-    nk1(:,i) = T*neck1tip_point;
-    nk2(:,i) = T*neck2tip_point;
-    neck1tip_point = nk1(:,i);
-    neck2tip_point = nk2(:,i);
+    PT = PT(1:3, 1:3);
+for rindex = 1:length(roll_range)
+    % Create roll matrix
+    r = roll_range(rindex)*pi/180;
+    RT = makehgtform('xrotate',r);
+    RT = RT(1:3, 1:3);
+
+    % Apply pitch transform. Then roll transform. To the tips of the linear
+    % actuators.
+    neck1tip_point = RT*PT*neck1tip_point_orig;
+    neck2tip_point = RT*PT*neck2tip_point_orig;
     
-    neck1(:,pidx,ridx) = neck1tip_point - neck1base_point;
-    neck2(:,pidx,ridx) = neck2tip_point - neck2base_point;
+    % Store for plotting
+
     
-%     figure(1)
-%     cla
-%     draw_neck
-%     line(nk1(1,:),nk1(2,:),nk1(3,:))
-%     line(nk2(1,:),nk2(2,:),nk2(3,:))
-%     pause(0.05)
+    % Compute vector of linear actuator
+    neck1(:,pindex,rindex) = neck1tip_point - neck1base_point;
+    neck2(:,pindex,rindex) = neck2tip_point - neck2base_point;
+    
+    % Plot neck
+    if mod(pindex,10)==0
+        if mod(rindex,10)==0
+            figure(1)
+            cla
+            draw_neck
+            % Plot surface of reachable area
+            i = i + 1;
+            nk1(:,i) = neck1tip_point;
+            nk2(:,i) = neck2tip_point;        
+            line(nk1(1,:),nk1(2,:),nk1(3,:))
+            line(nk2(1,:),nk2(2,:),nk2(3,:))
+        %     pause(0.05)
+        end
+    end
 end
+    % Insert discontinuities in the plotted lines
     i = i + 1;
     nk1(:,i) = NaN;
     nk2(:,i) = NaN;
 end
 
 % Compute lengths of vectors
-map1 = squeeze(sqrt(neck1(1,:,:).^2 + neck1(2,:,:).^2 + neck1(3,:,:).^2));
-map2 = squeeze(sqrt(neck2(1,:,:).^2 + neck2(2,:,:).^2 + neck2(3,:,:).^2));
+map1 = squeeze(sqrt(sum(neck1.^2)));
+map2 = squeeze(sqrt(sum(neck2.^2)));
 
 save('map1.mat','map1')
 save('map2.mat','map2')
--- Neck_science/reset_neck_coordinates.m
+++ Neck_science/reset_neck_coordinates.m
@@ -8,21 +8,26 @@
 adjleg = 29.72;
 opleg = 45;
 height = 93.05;
-
-a = adjleg;
-b = adjleg;
-c = opleg;
-
-lambda = acos((a^2+b^2-c^2)./(2*a*b));
-h = cos(lambda/2)*adjleg;
-e = sin(lambda/2)*adjleg;
+              % _______c_______
+a = adjleg;   % \             /
+b = adjleg;   %   \         /    * = lambda
+c = opleg;    %  a  \     /  b            
+              %       \*/                % ___e___
+lambda = acos((a^2+b^2-c^2)./(2*a*b));   % \      |
+h = cos(lambda/2)*adjleg;                %   \    |
+e = sin(lambda/2)*adjleg;                %     \  | h
+                                         %       \|     
 
 % Sanity check
 assert(abs((h^2+e^2)/(adjleg^2) - 1) < 0.00001)
 
-% Coordinates of interest
+% Coordinates of interest [x; y; z]
 universal_point = [0; 0; 0];
 neck1tip_point = universal_point + [-h; -e; 0];
 neck2tip_point = universal_point + [-h; +e; 0];
 neck1base_point = universal_point + [0; -e; -height];
 neck2base_point = universal_point + [0; e; -height];
+
+% For resetting
+neck1tip_point_orig = neck1tip_point;
+neck2tip_point_orig = neck2tip_point;
