a1ab9990e9052bd0866d58b5ade44dec2f2dd4c2
William Hilton <wmhilton@gmail.com>
2013-12-06 00:45:07 -0500

    Fixed installer with correct path to docpad.

--- install_all.sh
+++ install_all.sh
@@ -1,6 +1,6 @@
 set -e
 npm install
-docpad generate --env static
+./node_modules/.bin/docpad generate --env static
 cd hubo-ach_server
 ./install.sh
 echo "Copy the contents of drchubo.js/out/drc to Apache."
