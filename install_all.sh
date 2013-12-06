8afc701f2ecc7859a3d0bf0a65c27ca114b2b745
William Hilton <wmhilton@gmail.com>
2013-12-06 00:57:15 -0500

    Modified ./install_all.sh to copy output to /var/www/hubo

--- install_all.sh
+++ install_all.sh
@@ -3,4 +3,4 @@ npm install
 ./node_modules/.bin/docpad generate --env static
 cd hubo-ach_server
 ./install.sh
-echo "Copy the contents of drchubo.js/out/drc to Apache."
+cp -R ./out/* /var/www/hubo
